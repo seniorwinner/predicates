ddda9988edd724c2435e33098425b67ca2805ea5
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2021-01-07 00:39:22 +0300

    done chapter 005

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,92 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (if (= nil string)
+    false
+    (let [l (count string)]
+       (if (== 0 l)
+         true
+         (every? whitespace? string)))))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [x] (has-award? book x)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter (fn [x] (not (false? x))) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (not (my-some (complement pred) a-seq)))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (== 0 (mod n x)))]
+    (not (some pred (range 2 n)))))
 ;^^
+
+(def graphic-novels [{:name "Yotsuba 1" :series "Yotsuba"}
+                     {:name "Yotsuba 5" :series "Yotsuba"}
+                     {:name "Persepolis"}
+                     {:name "That Yellow Bastard" :series "Sin City"}
+                     {:name "The Hard Goodbye" :series "Sin City"}
+                     {:name "Maus"}
+                     {:name "Solanin"}
+                     {:name "Monster 3" :series "Monster"}])
+
+(defn key->predicate [a-key]
+  (fn [a-map] (contains? a-map a-key)))
+
+(defn non-negatives [a-seq]
+  (let [non-negative? (fn [n] (not (neg? n)))]
+    (filter non-negative? a-seq)))
+
+(defn non-negatives-alt [a-seq]
+  (filter (complement neg?)))
+
+;(defn complement [predicate]
+;  (fn [x] (not predicate x)))
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def authors #{china, octavia, kay, dick, zelazny})
+
+(def cities {:title "The City and the City" :authors #{china}
+             :awards #{:locus, :world-fantasy, :hugo}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
+                    :awards #{:hugo}})
+(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
+(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
+(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
+
+(def books #{cities, wild-seed, lord-of-light,
+             deus-irae, ysabel, scanner-darkly})
+
